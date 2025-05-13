5c73fb72d880a803043e114d49dd2ef06b04087f
George Sabo <george.sabo@edu.sait.ca>
2025-05-13 11:26:03 -0600

    Week2 submission fix
    fixed a couple thing, add the week3 link. added a home button to week3 page and did some styling to home page.

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-4xl pb-5">CPRG 306: Web Development 2 - Assignments</h1>
       <ul>
         <li><Link href="/week-2" className="text-cyan-600 underline hover:text-cyan-300">Week 2 - Assignment</Link></li>
-        <li><Link href="/week-3" className="text-cyan-600 underline hover:text-cyan-300">{/*Week 3 - Assignment*/}</Link></li>
+        <li><Link href="/week-3" className="text-cyan-600 underline hover:text-cyan-300">Week 3 - Assignment</Link></li>
         <li><Link href="/week-3" className="text-cyan-600 underline hover:text-cyan-300">{/*Week 4 - Assignment*/}</Link></li>
         <li><Link href="/week-4" className="text-cyan-600 underline hover:text-cyan-300">{/*Week 5 - Assignment*/}</Link></li>
         <li><Link href="/week-5" className="text-cyan-600 underline hover:text-cyan-300">{/*Week 6 - Assignment*/}</Link></li>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,15 @@
+import Link from "next/link";
 import ItemList from "./item-list";
 
 export default function Page() {
     return (
         <main>
             <h1 className="text-5xl p-4 font-bold text-amber-100 bg-indigo-950 border-blue-950 border-4 m-2 w-fit ">Shopping List</h1>
+            <div className="p-3">
+                <Link href="/" className="w-fit border-4 border-blue-800 bg-blue-800 p-1 ">Back to Home</Link>
+            </div>
             <ItemList />
+            <Link href="/" className="w-fit border-4 border-blue-800 bg-blue-800 p-1 ">Back to Home</Link>
         </main>
     )
 }
