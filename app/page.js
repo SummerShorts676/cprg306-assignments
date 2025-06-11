2ac0ec76ee9b3b7bab32ca90a53501d2186dce6d
George Sabo <george.sabo@edu.sait.ca>
2025-06-11 12:30:07 -0600

    week5 assignment
    created week five lab link in home page.js and added the two files mentioned in the documentation for this assignment.
    add new-item and page.js in week-5 folder, made sure it match properly and displayed the new-item component in the wee5-5/page.js file

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         <li><Link href="/week-2" className="text-cyan-600 underline hover:text-cyan-300">Week 2 - Assignment</Link></li>
         <li><Link href="/week-3" className="text-cyan-600 underline hover:text-cyan-300">Week 3 - Assignment</Link></li>
         <li><Link href="/week-4" className="text-cyan-600 underline hover:text-cyan-300">Week 4 - Assignment</Link></li>
-        <li><Link href="/week-5" className="text-cyan-600 underline hover:text-cyan-300">{/*Week 5 - Assignment*/}</Link></li>
+        <li><Link href="/week-5" className="text-cyan-600 underline hover:text-cyan-300">Week 5 - Assignment</Link></li>
         <li><Link href="/week-6" className="text-cyan-600 underline hover:text-cyan-300">{/*Week 6 - Assignment*/}</Link></li>
         <li><Link href="/week-7" className="text-cyan-600 underline hover:text-cyan-300">{/*Week 7 - Assignment*/}</Link></li>
         <li><Link href="/week-8" className="text-cyan-600 underline hover:text-cyan-300">{/*Week 8 - Assignment*/}</Link></li>
---
+++ app/week-5/page.js
@@ -0,0 +1,15 @@
+import NewItem from "./new-item";
+
+
+
+
+export default function WeekFivePage() {
+
+
+
+    return (
+        <main> 
+            <NewItem />
+        </main>
+    )
+}
